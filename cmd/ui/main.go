f987ed9f6636ec334047ea73f7d897cfc346796d
m0rfes <fahimkhan20148@gmail.com>
2025-01-24 06:25:02 +0400

    feat: add UI service with Dockerfile, Helm charts, and basic routing

---
+++ cmd/ui/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/M0rfes/go-chat-ms/ui/controllers"
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+)
+
+func createRender() multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+	r.AddFromFiles("login", "templates/base.html", "templates/index.html")
+	return r
+}
+
+func main() {
+	router := gin.Default()
+
+	// Set up templating engine
+	router.HTMLRender = createRender()
+
+	// Routes
+	router.GET("/", controllers.IndexPage)
+
+	// Start the server
+	router.Run(":8080")
+}
